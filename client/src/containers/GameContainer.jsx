e31e5c0d5523b693967b7270bbd31b2aebf65a47
Ross Melville <rossmelville@hotmail.com>
2017-09-27 10:28:45 +0100

    Initial commit. Added Container folder with GameContainer file, components folder with Board.jsx and Square.jsx files.

---
+++ client/src/containers/GameContainer.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import Board from '../components/Board.jsx';
+import Square from '../components/Square.jsx';
+
+class GameContainer extends Component {
+
+
+}
+
+export default GameContainer

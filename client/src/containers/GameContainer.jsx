af7030ae62cd358a618563942c69e2e8cc8980ac
Ross Melville <rossmelville@hotmail.com>
2017-09-27 10:30:58 +0100

    Render function added to GameContainer

--- client/src/containers/GameContainer.jsx
+++ client/src/containers/GameContainer.jsx
@@ -4,6 +4,14 @@ import Square from '../components/Square.jsx';
 
 class GameContainer extends Component {
 
+  render() {
+    return (
+      <section className="game">
+        Welcome to Tic-Tac-Toe
+      </section>
+    )
+  }
+
 
 }
 

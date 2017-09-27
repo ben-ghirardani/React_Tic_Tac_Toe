780281f3ca48e15d491bf32810f50b9e6a2fa335
Ross Melville <rossmelville@hotmail.com>
2017-09-27 10:43:40 +0100

    Added constructor to GameContainer with state.

--- client/src/containers/GameContainer.jsx
+++ client/src/containers/GameContainer.jsx
@@ -4,6 +4,15 @@ import Square from '../components/Square.jsx';
 
 class GameContainer extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedPositions = Array(9).fill(null),
+      turn = "a",
+      winningCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    }
+  }
+
   render() {
     return (
       <section className="game">

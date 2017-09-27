3b721021dde9243ca9de3234bf5ff8bd9f7b4b5b
Ross Melville <rossmelville@hotmail.com>
2017-09-27 10:56:49 +0100

    button added yay

---
+++ client/src/components/Square.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Square extends Component {
+
+  render(){
+    return(
+      <button>
+        Our buttons
+      </button>
+      )
+  }
+
+}
+
+export default Square;
